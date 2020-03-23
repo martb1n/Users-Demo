56d81c878e3e92302a6c911c8dda428873eaa188
recamadasj <jrecamadas@stackify.com>
2020-03-23 13:11:08 +0800

    Users Demo
    University Module

--- Users-Demo/Controllers/UniversityController.cs
+++ Users-Demo/Controllers/UniversityController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using Users_Demo.DAL.Models;
 using Users_Demo.Handler.University.Request;
-using Users_Demo.Handler.User.Request;
 
 namespace Users_Demo.Controllers
 {
